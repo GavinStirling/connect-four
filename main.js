a0c1ad318dcfb7e398accc24b4b9ef56a807c874
Gavin Stirling <gavinstir@gmail.com>
2022-05-31 09:14:43 +0100

    Reset event istener doesn't work

--- main.js
+++ main.js
@@ -1,41 +1,45 @@
 // Declaring variables which connect the JS and HTML
 const gameBoard = document.querySelector(".game__board");
 const columns = document.getElementsByClassName("game__columns");
-const reset = document.querySelector(".game__reset");
+const newGameButton = document.querySelector(".game__reset");
 
 const setGrid = (gridSize) => {
     console.log("setGrid called");
-
     const board = [];
     const bools = Array(gridSize).fill(false);
     board.push(bools);
-
     for (let index = 0; index < gridSize; index++) {
         const array = Array(gridSize).fill(0);
         board.push(array);
     }
-
-    // board.unshift(bools);
     return board;
 };
 
 const setBoard = (boardArr) => {
+    gameBoard.innerHTML = "";
     console.log("setBoard called");
     for (let index = 0; index < boardArr[0].length; index++) {
-        gameBoard.innerHTML += `<div class="game__columns" style = "
-        grid-column: ${index + 1};
-        grid-row: ${1};
-        ">Col. ${index + 1}</div>`;
+        gameBoard.innerHTML += columnLabels(index);
     }
-
     for (let x = 1; x < boardArr.length; x++) {
         for (let y = 0; y < boardArr[x].length; y++) {
-            gameBoard.innerHTML += `<div id="Block-${x}-${y}" class="game__blocks" style = "
+            gameBoard.innerHTML += blockLabels(x, y);
+        }
+    }
+};
+
+const columnLabels = (index) => {
+    return `<div class="game__columns" style = "
+  grid-column: ${index + 1};
+  grid-row: ${1};
+  ">Col. ${index + 1}</div>`;
+};
+
+const blockLabels = (x, y) => {
+    return `<div id="Block-${x}-${y}" class="game__blocks" style = "
         grid-column: ${y + 1};
         grid-row: ${x + 1};"
         >Block ${x + 1} ${y + 1}</div>`;
-        }
-    }
 };
 
 // -------------------- Game Play variables and Functions ---------------------------- //
@@ -44,13 +48,16 @@ const setBoard = (boardArr) => {
 const gridSize = 5;
 let turns = 1;
 const boardArr = setGrid(gridSize);
-setBoard(boardArr);
 console.log(boardArr);
+setBoard(boardArr);
 
-const newGame = (gridSize) => {
-    console.log("newGame called");
-    setGrid(gridSize);
-    setBoard(gridSize);
+const newGame = () => {
+    console.log("newGame Called");
+    turns = 1;
+    const newGrid = setGrid(gridSize);
+    setBoard(boardArr);
+    console.log(newGrid);
+    return newGrid;
 };
 
 const selectColumn = (columnText) => {
@@ -79,21 +86,16 @@ const placeDisc = (event) => {
     const row = selectRow(column);
     const targetDiv = document.querySelector(`#Block-${row - 1}-${column - 1}`);
 
-    // console.log("Selected column no. " + column);
-    // console.log(row, column);
-
     if (!boardArr[0][column - 1]) {
+        targetDiv.innerText = "";
         if (turns % 2 === 1) {
             boardArr[row - 1][column - 1] = 1;
-            // console.log(boardArr);
             targetDiv.innerHTML += insertPlayerDiscHTML(row, column);
-            checkWin(); 
         } else {
             boardArr[row - 1][column - 1] = 2;
-            console.log(boardArr);
             targetDiv.innerHTML += insertComputerDiscHTML(row, column);
-            checkWin();
         }
+        checkWin();
         turns += 1;
     } else {
         alert("This column is full. Please select another.");
@@ -101,61 +103,81 @@ const placeDisc = (event) => {
 };
 
 const insertPlayerDiscHTML = (row, column) => {
-    return `<div class="game__player-disc"style = "grid-column: ${column}; grid-row: ${row};">Disc</div>`;
+    return `<div class="game__player-disc"style = "grid-column: ${column}; grid-row: ${row};"></div>`;
 };
 
 const insertComputerDiscHTML = (row, column) => {
-    return `<div class="game__computer-disc"style = "grid-column: ${column}; grid-row: ${row};">Disc</div>`;
+    return `<div class="game__computer-disc"style = "grid-column: ${column}; grid-row: ${row};"></div>`;
 };
 
-const checkLine = (a,b,c,d) => {
-  return ((a != 0) && (a == b) && (a == c) && (a == d))
-}
+const checkLine = (a, b, c, d) => {
+    return a != 0 && a == b && a == c && a == d;
+};
 
-
-
-
-
+// Checking if a player has lined up four in a row
 const checkWin = () => {
-  // Check down
-  for (let row = 0; row < gridSize-2; row++) {
-    for (let column = 0; column < boardArr[row].length; column++) {
-      if (checkLine(boardArr[row][column],boardArr[row+1][column],boardArr[row+2][column],boardArr[row+3][column])) {
-        alert(`Player ${boardArr[row][column]} wins!`)
-      }
+    // Check down
+    for (let row = 0; row < gridSize - 2; row++) {
+        for (let column = 0; column < boardArr[row].length; column++) {
+            if (
+                checkLine(
+                    boardArr[row][column],
+                    boardArr[row + 1][column],
+                    boardArr[row + 2][column],
+                    boardArr[row + 3][column]
+                )
+            ) {
+                alert(`Player ${boardArr[row][column]} wins!`);
+            }
+        }
     }
-  }
-  // Check right
-  for (let row = 0; row < boardArr.length; row++) {
-    for (let column = 0; column < boardArr[row].length-2; column++) {
-      if (checkLine(boardArr[row][column],boardArr[row][column+1],boardArr[row][column+2],boardArr[row][column+3])) {
-        alert(`Player ${boardArr[row][column]} wins!`)
-      }
+    // Check right
+    for (let row = 0; row < boardArr.length; row++) {
+        for (let column = 0; column < boardArr[row].length - 2; column++) {
+            if (
+                checkLine(
+                    boardArr[row][column],
+                    boardArr[row][column + 1],
+                    boardArr[row][column + 2],
+                    boardArr[row][column + 3]
+                )
+            ) {
+                alert(`Player ${boardArr[row][column]} wins!`);
+            }
+        }
     }
-  }
+    // Check down and right
+    for (let row = 0; row < gridSize - 2; row++) {
+        for (let column = 0; column < boardArr[row].length - 2; column++) {
+            if (
+                checkLine(
+                    boardArr[row][column],
+                    boardArr[row + 1][column + 1],
+                    boardArr[row + 2][column + 2],
+                    boardArr[row + 3][column + 3]
+                )
+            ) {
+                alert(`Player ${boardArr[row][column]} wins!`);
+            }
+        }
+    }
 
-  // Check down and right
-  for (let row = 0; row < gridSize-2; row++) {
-    for (let column = 0; column < boardArr[row].length-2; column++) {
-      if (checkLine(boardArr[row][column],boardArr[row+1][column+1],boardArr[row+2][column+2],boardArr[row+3][column+3])) {
-        alert(`Player ${boardArr[row][column]} wins!`)
-      }
+    // Check down and left
+    for (let row = gridSize - 1; row <= gridSize; row++) {
+        for (let column = 0; column < boardArr[row].length - 2; column++) {
+            if (
+                checkLine(
+                    boardArr[row][column],
+                    boardArr[row - 1][column + 1],
+                    boardArr[row - 2][column + 2],
+                    boardArr[row - 3][column + 3]
+                )
+            ) {
+                alert(`Player ${boardArr[row][column]} wins!`);
+            }
+        }
     }
-  }
-
-  // Check down and left
-  for (let row = gridSize-1; row <= gridSize; row++) {
-    for (let column = 0; column < boardArr[row].length-2; column++) {
-      if (checkLine(boardArr[row][column],boardArr[row-1][column+1],boardArr[row-2][column+2],boardArr[row-3][column+3])) {
-        alert(`Player ${boardArr[row][column]} wins!`)
-      }
-    }
-  }
-
 };
-
-
-
 
 // ---------------------------- Event Listeners ------------------------------------ //
 
@@ -165,4 +187,4 @@ columnsArray.forEach((column) => {
     column.addEventListener("click", placeDisc);
 });
 
-// reset.addEventListener("click", newGame(5));
+newGameButton.addEventListener("click", newGame);
